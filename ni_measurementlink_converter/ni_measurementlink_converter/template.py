022b4fa854660a2663d1ab51bbd5ccb81060944b
Avinash2Suresh <avinash2.suresh@ni.com>
2024-04-23 15:31:33 +0530

    fix: template for basic measurement

--- ni_measurementlink_converter/ni_measurementlink_converter/template.py
+++ ni_measurementlink_converter/ni_measurementlink_converter/template.py
@@ -3,9 +3,11 @@
 import logging
 import pathlib
 import re
-from typing import Any, Optional, Tuple
+from typing import Any
+import shutil
 
 import click
+from pathlib import Path
 from mako import exceptions
 from mako.template import Template
 
@@ -46,12 +48,94 @@ def _logging(verbose:bool):
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=level)
 
 
+def _get_input_configurations(params):
+    extracted_params = {}
+    for param in params:
+        parts = param.split(':')
+        if len(parts) != 2:
+            raise click.BadParameter(f"Invalid parameter format: {param}. Please use the format 'name:type'")
+        name, param_type = parts
+        if param_type == 'int':
+            extracted_params[name] = 'nims.DataType.Int32'
+        elif param_type == 'bool':
+            extracted_params[name] = 'nims.DataType.Boolean'
+        elif param_type == 'float':
+            extracted_params[name] = 'nims.DataType.Double'
+        elif param_type == 'List[float]':
+            extracted_params[name] = 'nims.DataType.FloatArray1D'
+        elif param_type == 'List[bool]':
+            extracted_params[name] = 'nims.DataType.DoubleArray1D'
+        else:
+            raise click.BadParameter("Invalid parameter")
+    return extracted_params
+
+
+def _get_instrument(instrument_type):
+    if instrument_type == "nidcpower":
+        return 'nims.session_management.INSTRUMENT_TYPE_NI_DCPOWER'
+    else:
+        raise click.BadParameter(f"Invalid instrument")
+    
+
+def _generate_method_signature(inputs):
+    signature = ", ".join(inputs)
+    return signature
+
+
+def _get_parameter_string(inputs):
+    parameter_names = [param.split(":")[0] for param in inputs]
+    return ", ".join(parameter_names)
+
+
+def _get_all_types(inputs):
+    all_types = []
+    for param in inputs:
+        param_type = param.split(":")[1]
+        all_types.append(param_type)
+    return ", ".join(all_types)
+    
+
 @click.command()
 @click.argument("display_name")
+@click.option(
+    "-r",
+    "--resource_name",
+    help="Resource name should be acted as pin name",
+)
+@click.option(
+    "-t",
+    "--instrument_type",
+    help="Type of the instrument",
+)
+@click.option(
+    "-i",
+    "--inputs",
+    help="Input parameter in the format name:type",
+    multiple=True
+)
+@click.option(
+    "-o",
+    "--outputs",
+    help="output parameter in the format name:type",
+    multiple=True
+)
 def convert_measurement(
     display_name: str,
+    resource_name: str,
+    instrument_type: str,
+    inputs: str,
+    outputs: str,
 ) -> None:
     _logging(True)
+
+    input_configurations = _get_input_configurations(inputs)
+    output_configurations = _get_input_configurations(outputs)
+
+    instrument = _get_instrument(instrument_type)
+    input_signature = _generate_method_signature(inputs)
+    input_param_names = _get_parameter_string(inputs)
+
+    output_param_types = _get_all_types(outputs)
 
     service_class = f"{display_name}_Python"
     display_name_for_filenames = re.sub(r"\s+", "", display_name)
@@ -59,6 +143,10 @@ def convert_measurement(
     directory_out_path = pathlib.Path.cwd() / display_name_for_filenames
     directory_out_path.mkdir(exist_ok=True, parents=True)
     measurement_version = 1.0
+
+    source_file = "C:\\Users\\avinash.suresh\\Desktop\\Py utility\\Measurements\\Step2_Wrap.py"
+    new_name = 'Meas.py'
+    shutil.copy(str(source_file), str(directory_out_path) + str('\\') + new_name)
 
     _create_file(
         "measurement.py.mako",
@@ -68,6 +156,14 @@ def convert_measurement(
         version=measurement_version,
         service_class=service_class,
         serviceconfig_file=serviceconfig_file,
+        resource_name=resource_name,
+        instrument_type=instrument_type,
+        instrument=instrument,
+        input_configurations=input_configurations,
+        output_configurations=output_configurations,
+        input_signature = input_signature,
+        input_param_names=input_param_names,
+        output_param_types=output_param_types
     )
     _create_file(
         "measurement.serviceconfig.mako",
--- ni_measurementlink_converter/ni_measurementlink_converter/templates/measurement.py.mako
+++ ni_measurementlink_converter/ni_measurementlink_converter/templates/measurement.py.mako
@@ -1,10 +1,13 @@
-<%page args="display_name, version, service_class, serviceconfig_file"/>\
+<%page args="display_name, version, service_class, serviceconfig_file,resource_name,instrument_type,instrument,input_configurations,output_configurations,input_signature,input_param_names,output_param_types"/>\
 \
 """A default measurement with an array in and out."""
 
 import logging
 import pathlib
 import sys
+from typing import List, Tuple, Iterable
+import ${instrument_type}
+from Meas import measurement
 
 import click
 import ni_measurementlink_service as nims
@@ -14,16 +17,27 @@ service_directory = pathlib.Path(script_or_exe).resolve().parent
 measurement_service = nims.MeasurementService(
     service_config_path=service_directory / "${serviceconfig_file}",
     version="${version}",
-    ui_file_paths=[service_directory / test.measui],
+    ui_file_paths=[service_directory / "test.measui"],
 )
 
 
 @measurement_service.register_measurement
-@measurement_service.configuration("Array in", nims.DataType.DoubleArray1D, [0.0])
-@measurement_service.output("Array out", nims.DataType.DoubleArray1D)
-def measure(array_input):
-    array_output = array_input
-    return (array_output,)
+@measurement_service.configuration(
+    "pin_names",
+    nims.DataType.IOResourceArray1D,
+    ["${resource_name}"],
+    instrument_type=${instrument},
+)
+    %for name, nims_type in input_configurations.items():
+@measurement_service.configuration("${name}", ${nims_type})
+    %endfor
+    %for name, nims_type in output_configurations.items():
+@measurement_service.output("${name}", ${nims_type})
+    %endfor
+def measure(pin_names: Iterable[str],${input_signature}) -> Tuple[${output_param_types}]:
+    with measurement_service.context.reserve_session(pin_names) as reservation:
+        with reservation.initialize_${instrument_type}_session() as session_info:
+            return measurement(session_info.session, ${input_param_names})
 
 
 def main() -> None:
